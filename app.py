7c5a84961faf8286b1001f21f8c72d18d361fb8a
Abi satya <abiambis19@gmail.com>
2024-04-09 18:13:10 +0700

    Change Name

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def fetch_poster(suggestion):
 def recommend_books(nama_buku):
     list_buku = []
     buku_id = np.where(buku_pivot.index == nama_buku)[0][0]
-    distance, suggestion = model.kneighbors(buku_pivot.iloc[buku_id,:].values.reshape(1,-1),n_neighbors = 6)
+    distance, suggestion = model.kneighbors(buku_pivot.iloc[buku_id,:].values.reshape(1,-1),n_neighbors = 11)
     poster_url = fetch_poster(suggestion)
     for i in range(len(suggestion)):
         buku = buku_pivot.index[suggestion[i]]
@@ -48,6 +48,24 @@ buku_terpilih = st.selectbox(
 
 if st.button('Show Recommendation'):
     buku_rekomendasi, poster_url = recommend_books(buku_terpilih)
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(buku_rekomendasi[6])
+        st.image(poster_url[6])
+    with col2:
+        st.text(buku_rekomendasi[7])
+        st.image(poster_url[7])
+    with col3:
+        st.text(buku_rekomendasi[8])
+        st.image(poster_url[8]) 
+    with col4:
+        st.text(buku_rekomendasi[9])
+        st.image(poster_url[9])
+    with col5:
+        st.text(buku_rekomendasi[10])
+        st.image(poster_url[10])
+    
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
